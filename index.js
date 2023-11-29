8672431ab9f17b4ff8986ad91c60532b49d48192
allonsg <allonsy053@gmail.com>
2023-11-29 22:55:47 +0200

    Rewrite to files

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const readline = require('readline');
 const crypto = require('crypto-js');
+const fs = require('fs');
+const path = require('path');
 
 function prompt(question) {
     const r = readline.createInterface({
@@ -29,12 +31,17 @@ async function promptEncryptionMode() {
 async function encryptDecryptData() {
     const sessionKey = await generateSessionKey();
     const mode = await promptEncryptionMode();
-    const data = await prompt('Please enter the data to encrypt: ');
+    const filePath = await prompt('Please enter the path to the file to encrypt: ');
+    const data = fs.readFileSync(path.resolve(__dirname, filePath), 'utf8');
     const encryptedData = crypto.AES.encrypt(data, sessionKey, {mode: crypto.mode[mode]}).toString();
     console.log('Encrypted data: ', encryptedData);
 
+    fs.writeFileSync(path.resolve(__dirname, 'encrypted.txt'), encryptedData, 'utf8');
+
     const decryptedData = crypto.AES.decrypt(encryptedData, sessionKey, {mode: crypto.mode[mode]}).toString(crypto.enc.Utf8);
     console.log('Decrypted data: ', decryptedData);
+
+    fs.writeFileSync(path.resolve(__dirname, 'decrypted.txt'), decryptedData, 'utf8');
 }
 
 encryptDecryptData();
